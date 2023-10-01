37258d472a75b6b5deba389eb6dea44c0e0ef7e1
talithaaulia <72477041+talithaaulia@users.noreply.github.com>
2023-10-01 11:01:05 +0700

    Add files via upload

---
+++ event_handler.js
@@ -0,0 +1,38 @@
+// PENERAPAN CLASS COMPONENT
+// Button Component
+class MyButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleButtonClick() {
+        alert(this.props.message);
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleButtonClick.bind(this)} style={{ margin: "10px" }}>
+                {this.props.children}
+            </button>
+        );
+    }
+}
+
+// App Component
+class MyApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Learn React</h1>
+                <MyButton message="Learn React">React</MyButton>
+                <MyButton message="Learn JavaScript">JavaScript</MyButton>
+            </div>
+        );
+    }
+}
+
+const myElement = <MyApp />;
+const myApp = myElement;
+
+ReactDOM.createRoot(document.getElementById('root')).render(myApp);
+
